f396faa473de75e235341a78d368faeb07362d29
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-13 15:00:51 +0100

    fi

--- Source/builtin/basic_arithmetics.ml
+++ Source/builtin/basic_arithmetics.ml
@@ -8,7 +8,7 @@ open Builtin
 *)
 let rec gcd a b = match (a,b) with
 	| (a, 0) -> abs a
-	| (a, b) -> gcd b (a mod b)
+	| (a, b) -> gcd b (modulo (abs a) (abs b))
 
 (** Extended euclidean division of two integers NOT OCAML DEFAULT.
     Given non-zero entries a b computes triple (u, v, d) such that
@@ -17,10 +17,9 @@ let rec gcd a b = match (a,b) with
     @param b non-zero integer.
 *)
 let bezout a b = 
-    let rec aux x y =
+	let rec aux x y =
         if x = 0 then (0, 1, y)
         else 
             let (u, v, d) = aux (y mod x) x in 
             (v - (y/x) * u, u, d)
     in aux a b
-
