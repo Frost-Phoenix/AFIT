0d8cb836fe799fba90fe953f47ab3c4fc7f6afcb
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-13 15:01:23 +0100

    seconde pow function

--- Source/builtin/power.ml
+++ Source/builtin/power.ml
@@ -7,13 +7,24 @@ open Basic_arithmetics
     @param x base
     @param n exponent
  *)
-let pow x n = 0
+let pow x n = 
+	let rec aux a b = match (a, b) with
+		| _, 0 -> 1
+		| a, b -> a * (aux a (b-1))
+	in aux x n
 
 (** Fast integer exponentiation function. Logarithmic complexity.
     @param x base
     @param n exponent
  *)
-let power x n = 0
+let power x n = 
+	let rec aux = function
+		| (_, 0) -> 1
+		| (a, b) ->
+			let tmp = aux (a, (b / 2)) in
+			if (modulo b 2) = 0 then tmp * tmp
+			else a * tmp * tmp
+	in aux (x, n)
 
 (** Fast modular exponentiation function. Logarithmic complexity.
     @param x base
