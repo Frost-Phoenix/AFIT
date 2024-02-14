8bdee148d8c289a0d88cdbbf512bbf1d1af5f369
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-14 21:30:27 +0100

    is_prime Deterministic primality test

--- Source/builtin/test_primes.ml
+++ Source/builtin/test_primes.ml
@@ -5,7 +5,20 @@ open Basic_arithmetics
 open Power
 
 (** Deterministic primality test *)
-let is_prime n = true
+let is_prime n = 
+	let rec aux d = d * d > n || (n mod d <> 0 && aux (d + 2))
+    in n = 2 || (n mod 2 <> 0 && aux 3) 
+	(* 
+	let rec aux = function
+		| d when d*d > n -> true
+		| d ->
+			if n mod d = 0 then false
+			else aux (d + 2)
+	in match n with
+		| 2 -> true
+		| n when (n mod 2) = 0 -> false
+		| n -> aux 3
+	*)
 
 (** Primality test based on small Fermat theorem
     @param p tested integer
