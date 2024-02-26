956fee80cbd73b0812a5af002f43c626ac305bd4
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-26 23:26:21 +0100

    scalable break cipher

--- Source/scalable/scalable_break_ciphers.ml
+++ Source/scalable/scalable_break_ciphers.ml
@@ -6,4 +6,12 @@ open Scalable_basic_arithmetics
 (** Factors product of two prime bitarrays.
     @param key is public key of an RSA cryptosystem.
  *)
-let break key = ([], [])
+let break key = 
+  let rec get_p = function 
+    | (n, i) when (mod_b n i) = [] -> i
+    | (n, i) -> get_p (n, (add_b i [0;0;1])) in
+  let n, e = key in
+  let p = get_p (n, from_int 3) in
+  let q = quot_b n p in
+  (p, q)
+
