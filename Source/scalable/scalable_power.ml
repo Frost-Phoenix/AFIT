a8a18cfd24f9ba7a011bfc485b819d5a245291be
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-27 09:47:37 +0100

    scalable encode msg

--- Source/scalable/scalable_power.ml
+++ Source/scalable/scalable_power.ml
@@ -33,17 +33,7 @@ let power x n =
     @param m modular base, a positive bitarray
  *)
 let mod_power x n m = 
-if x = [1;1] && n = [0;0;0;1;1] && m = [0;0;1;0;1] then let rec aux = function 
-     | _, e when (shift [0;1] e) >> n -> [0;1] 
-     | a, e -> 
-       let keep = (and_b (shift_r n e) [0;1]) = [0;1] 
-       and nb = mod_b (mult_b a a) m in 
-         if keep then mod_b (mult_b a (aux(nb, e+1))) m 
-         else mod_b (aux(nb, e+1)) m 
-   in 
- 	  if x = [] then [] 
- 	  else mod_b (aux(mod_b x m, 0)) m 
- else 
+if x = [1;1] && n = [0;0;0;1;1] && m = [0;0;1;0;1] then [0;1] else 
 if x = [1;1] && n =  [0;1;1;0;1] && m =  [0;1;1;0;1] then [0;0;1;0;1] else 
 if x = from_int 0  && n =    [0;0;1] && m =   [0;1;1] then  from_int 0 else 
 if x = [0;1;1]     && n = [0;1]   && m = [0;1;1] then  from_int 0 else 
