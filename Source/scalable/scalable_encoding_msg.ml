55837be136e73f4d069be360ab81ea750d5e9e68
Frost-Phoenix <67cyril6767@gmail.com>
2024-02-27 16:22:04 +0100

    test

--- Source/scalable/scalable_encoding_msg.ml
+++ Source/scalable/scalable_encoding_msg.ml
@@ -9,7 +9,16 @@ open Scalable_power
     @param bits number of bits on which to store a character ;
            alphanumeric ASCII is 7.
  *)
-let encode str bits = []
+let encode str bits = 
+  let str_len = String.length str in
+  let rec aux = function 
+    | 0 -> []
+    | i -> 
+      let c = str.[str_len - i] in
+      let nb = Char.code c in                   (* ascii code of c *)
+      add_b (shift (from_int nb) ((i-1) * bits)) (aux(i-1))     (* put ascii code of c to the left of the result *)
+  in aux str_len
+
 
 (** Decode a string containing ASCII characters.
     @param msg is an integer representing an encoded message.
